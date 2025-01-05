7518f784cded1ef08f3fd54ce331a1560b9140cd
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-01-05 12:11:04 -0500

    flagisms

--- build.bat
+++ build.bat
@@ -1,11 +1,14 @@
 @echo off
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 if not exist "bin" (
   mkdir "bin"
 )
 
 cl  /std:c++20 /O2 fs_bench.cpp /Fe .\bin\fs_bench.exe
+
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo "Built .\bin\fs_bench.exe"
