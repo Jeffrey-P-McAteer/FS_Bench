f6fff0627c2d07f1fd326559f01628361a737d1c
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-01-05 13:41:45 -0500

    If we don't find a community vcvarsall, call the enterprise file location

--- build.bat
+++ build.bat
@@ -4,6 +4,10 @@ where /q cl.exe
 IF ERRORLEVEL 1 (
   call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 )
+where /q cl.exe
+IF ERRORLEVEL 1 (
+  call "C:\Program Files\Microsoft Visual Studio\2022\VC\Auxiliary\Build\vcvarsall.bat" x64
+)
 
 if not exist "bin" (
   mkdir "bin"
